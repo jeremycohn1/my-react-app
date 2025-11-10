2021347d79b9f393e4cea3b9b1f082858902bb77
Jeremy Cohn <cjeremy@wustl.edu>
2025-11-10 11:23:51 -0600

    when handle click--> an X is added to the clicked square.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
+  }
 
   return (
-    <button 
+    <button
       className="square"
       onClick={handleClick}
     >
